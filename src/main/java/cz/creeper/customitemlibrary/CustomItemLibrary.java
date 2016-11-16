ad64ee3699b7c90596621a75216006b19cee353b
Jakub Hlusička <jakub.hlusicka@email.cz>
2016-11-16 21:17:29 +0100

    actually load and save definitions

--- src/main/java/cz/creeper/customitemlibrary/CustomItemLibrary.java
+++ src/main/java/cz/creeper/customitemlibrary/CustomItemLibrary.java
@@ -5,15 +5,14 @@ import cz.creeper.customitemlibrary.data.CustomItemData;
 import cz.creeper.customitemlibrary.data.CustomItemManipulatorBuilder;
 import cz.creeper.customitemlibrary.data.ImmutableCustomItemData;
 import cz.creeper.customitemlibrary.registry.CustomItemService;
+import cz.creeper.customitemlibrary.registry.CustomItemServiceImpl;
 import lombok.Getter;
 import ninja.leaping.configurate.ConfigurationOptions;
 import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.game.state.GameConstructionEvent;
-import org.spongepowered.api.event.game.state.GameInitializationEvent;
-import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+import org.spongepowered.api.event.game.state.*;
 import org.spongepowered.api.plugin.Plugin;
 
 import java.nio.file.Path;
@@ -35,6 +34,8 @@ public class CustomItemLibrary {
     private Logger logger;
     @Getter @Inject @DefaultConfig(sharedRoot = false)
     private Path configPath;
+    @Getter
+    private CustomItemService service;
 
     @Listener
     public void onGameConstruction(GameConstructionEvent event) {
@@ -49,7 +50,25 @@ public class CustomItemLibrary {
     @Listener
     public void onGameInitialization(GameInitializationEvent event) {
         logger.info("Loading CustomItemLibrary...");
+
+        service = new CustomItemServiceImpl();
+
+        service.loadDictionary();
+        Sponge.getServiceManager().setProvider(this, CustomItemService.class, service);
+
         logger.info("CustomItemLibrary loaded.");
+    }
+
+    @Listener
+    public void onGamePostInitialization(GamePostInitializationEvent event) {
+        // During this phase, plugins using this library should register their custom item definitions.
+    }
+
+    @Listener
+    public void onGameLoadComplete(GameLoadCompleteEvent event) {
+        logger.info("Saving CustomItemLibrary...");
+        service.saveDictionary();
+        logger.info("CustomItemLibrary saved.");
     }
 
     public static ConfigurationOptions getDefaultConfigurationOptions() {
