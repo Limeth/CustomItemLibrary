36dabce36a82b0a946c9193ed51901e265026323
Jakub Hlusička <jakub.hlusicka@email.cz>
2016-12-06 22:04:47 +0100

    more progress on materials

--- src/main/java/cz/creeper/customitemlibrary/item/material/CustomMaterial.java
+++ src/main/java/cz/creeper/customitemlibrary/item/material/CustomMaterial.java
@@ -1,6 +1,7 @@
 package cz.creeper.customitemlibrary.item.material;
 
 import cz.creeper.customitemlibrary.item.AbstractCustomItem;
+import cz.creeper.mineskinsponge.SkinRecord;
 import org.spongepowered.api.item.inventory.ItemStack;
 
 public class CustomMaterial extends AbstractCustomItem<CustomMaterial, CustomMaterialDefinition> {
@@ -8,4 +9,26 @@ public class CustomMaterial extends AbstractCustomItem<CustomMaterial, CustomMat
         super(itemStack, definition);
     }
 
+    public String getTexture() {
+        CustomMaterialRegistry registry = CustomMaterialRegistry.getInstance();
+        String textureId = registry.getTextureId(
+                SkinRecord.of(getItemStack()).orElseThrow(
+                        () -> new IllegalStateException("Could not create a SkinRecord out of a CustomMaterial ItemStack.")
+                )
+        );
+        String pluginId = CustomMaterialDefinition.getPluginId(textureId);
+
+        if(!getDefinition().getPluginId().equals(pluginId))
+            throw new IllegalStateException("This plugin is not the owner of this texture. Have two plugins registered the same texture?");
+
+        return CustomMaterialDefinition.getTexture(textureId);
+    }
+
+    public void setTexture(String texture) {
+        CustomMaterialRegistry registry = CustomMaterialRegistry.getInstance();
+        String textureId = CustomMaterialDefinition.getTextureId(getDefinition().getPluginId(), texture);
+        SkinRecord skin = registry.getSkin(textureId);
+
+        skin.apply(getItemStack());
+    }
 }
