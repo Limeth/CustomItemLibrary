083c111f64750ec3c15e7007a1232e8008926a01
Jakub Hlusička <limeth@protonmail.com>
2017-01-11 15:14:23 +0100

    store the player's ID in MiningStopEvent instead of an Optional

--- src/main/java/cz/creeper/customitemlibrary/managers/MiningManager.java
+++ src/main/java/cz/creeper/customitemlibrary/managers/MiningManager.java
@@ -94,7 +94,7 @@ public class MiningManager {
             if(durationTicks < 0)
                 durationTicks = 0;
 
-            MiningStopEvent stopEvent = new MiningStopEvent(Optional.of(player), mining.snapshot, durationTicks,
+            MiningStopEvent stopEvent = new MiningStopEvent(playerId, mining.snapshot, durationTicks,
                     Cause.source(CustomItemLibrary.getInstance().getPluginContainer()).notifier(player).build(), MiningStopEvent.Reason.STARTED_MINING_OTHER_BLOCK);
 
             Sponge.getEventManager().post(stopEvent);
@@ -151,7 +151,7 @@ public class MiningManager {
                     if (cancelled) {
                         playerToMining.remove(playerId);
 
-                        MiningStopEvent stopEvent = new MiningStopEvent(player, mining.snapshot, durationTicks,
+                        MiningStopEvent stopEvent = new MiningStopEvent(playerId, mining.snapshot, durationTicks,
                                 Cause.source(CustomItemLibrary.getInstance().getPluginContainer()).notifier(player).build(), MiningStopEvent.Reason.MINING_PROGRESS_EVENT_CANCELLED);
 
                         Sponge.getEventManager().post(stopEvent);
@@ -161,7 +161,7 @@ public class MiningManager {
                 }
             }
 
-            MiningStopEvent stopEvent = new MiningStopEvent(player, mining.snapshot, durationTicks,
+            MiningStopEvent stopEvent = new MiningStopEvent(playerId, mining.snapshot, durationTicks,
                     Cause.source(CustomItemLibrary.getInstance().getPluginContainer()).notifier(player).build(), MiningStopEvent.Reason.BUTTON_RELEASED);
 
             Sponge.getEventManager().post(stopEvent);
